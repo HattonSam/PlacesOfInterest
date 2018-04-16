81efe5b827d6cbd0aed07397d88449657dd09180
HattonSam <32261145+HattonSam@users.noreply.github.com>
2018-04-16 11:01:27 +0100

    Adding the initial files

---
+++ www/js/index.js
@@ -0,0 +1,48 @@
+var find;
+var map;
+var loc;
+var path = [];
+
+
+       
+
+$(document).on("pageinit", "#pageone", function( ) {
+    
+    
+    find = navigator.geolocation.watchPosition(onLocationSuccess, onLocationFail)
+    
+});
+
+
+function onLocationSuccess(position) {
+    
+    loc = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    
+    position.coords.latitude; position.coords.longitude
+    
+    path.push(loc);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: loc,
+        zoom: 12
+    });
+    
+    
+     var mapPath = new google.maps.Polyline({
+          path: path,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+    
+    mapPath.setMap(map);
+    var marker = new google.maps.Marker ({
+        position: loc, 
+        title: "Your location",
+        map: map
+    })
+}
+
+function onLocationFail(error) {
+    console.log("Error: " + error.message);
+}
